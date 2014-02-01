fd3c25c0a0676d3a6bc730b6701de8bac8aa9bbd
poojavenkatram <poobhar3591@gmail.com>
2014-02-01 06:13:24 -0800

    Akshay's Commit

---
+++ PhoneApp2/PhoneApp2/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp2.Resources;
+
+namespace PhoneApp2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void OnSessionStateChanged(object sender, Facebook.Client.Controls.SessionStateChangedEventArgs e)
+        {
+            //this.ContentPanel.Visibility = (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened) ?
+              //                  Visibility.Visible : Visibility.Collapsed;
+            if (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Opened)
+            {
+                this.queryButton.Visibility = Visibility.Visible;
+                this.multiQueryButton.Visibility = Visibility.Visible;
+            }
+            else if (e.SessionState == Facebook.Client.Controls.FacebookSessionState.Closed)
+            {
+                this.queryButton.Visibility = Visibility.Collapsed;
+                this.multiQueryButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void queryButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fb = new Facebook.FacebookClient(this.loginButton.CurrentSession.AccessToken);
+            var result = await fb.GetTaskAsync("fql",
+                new
+                {
+                    q = "SELECT uid, name, pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 25)"
+                });
+
+            System.Diagnostics.Debug.WriteLine("Result: " + result.ToString());
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
