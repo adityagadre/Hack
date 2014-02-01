cacee322564c09b13a1e45b17d6cf225051859c7
yatinwadhawan <yatin.wadhawan@hotmail.com>
2014-02-01 09:27:54 -0800

    third_commit AK

--- PhoneApp2/PhoneApp2/MainPage.xaml.cs
+++ PhoneApp2/PhoneApp2/MainPage.xaml.cs
@@ -44,7 +44,10 @@ namespace PhoneApp2
                 {
                     q = "SELECT uid, name, pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me() LIMIT 25)"
                 });
-
+            ShellToast toast = new ShellToast();
+            toast.Title = "[title]";
+            toast.Content = "[content]";
+            toast.Show();
             System.Diagnostics.Debug.WriteLine("Result: " + result.ToString());
         }
 
